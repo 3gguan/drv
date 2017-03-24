8edabcf9e2b3ed89e4d6b904ef493d80d27d9158
3gguan@gmail.com <guan@debian.localdomain>
2017-03-24 23:09:29 +0800

    Committer: 3gguan@gmail.com <guan@debian.localdomain>
     On branch master
     Changes to be committed:
     Modify the directory

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Sun Jul 31 20:07:35 HKT 2016"
+#define LINUX_COMPILE_BY "guan"
+#define LINUX_COMPILE_HOST "debian"
+#define LINUX_COMPILER "gcc version 4.9.2 (Debian 4.9.2-10) "
